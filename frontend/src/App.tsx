89bbc2ef08ce6eb3323df4aa835e7da9bb52a99e
vm_ubuntu_pc_fisso <bttc.gabriele@gmail.com>
2025-09-03 19:00:08 +0200

    backup post google api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,27 +13,27 @@ import ActionPage from './pages/ActionPage';
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen pb-5">
-        <NavbarComponent />
-        <main className="flex-grow mt-3 mb-5">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-12">
-                <Routes>
-                  <Route path="/building-sites" element={<HomePage />} />
-                  <Route path="/register" element={<RegisterPage />} />
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/" element={<AboutPage />} />
-                  <Route path="/about" element={<AboutPage />} />
-                  <Route path="/profile" element={<ProfilePage />} />
-                  <Route path="/building-site-actions/:site_id" element={<BuildingSiteActionsPage />} />
-                  <Route path="/action-page/:link/:siteId/:date" element={<ActionPage />} />
-                </Routes>
+        <div className="flex flex-col min-h-screen pb-5">
+          <NavbarComponent />
+          <main className="flex-grow mt-3 mb-5">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-12">
+                  <Routes>
+                    <Route path="/building-sites" element={<HomePage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/" element={<AboutPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                    <Route path="/building-site-actions/:site_id" element={<BuildingSiteActionsPage />} />
+                    <Route path="/action-page/:link/:siteId/:date" element={<ActionPage />} />
+                  </Routes>
+                </div>
               </div>
             </div>
-          </div>
-        </main>
-      </div>
+          </main>
+        </div>
     </Router>
   );
 };
